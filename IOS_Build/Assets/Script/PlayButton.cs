9a180be805c4df406a711e0f87f54b21983c60e3
singitsck <69478063+singitsck@users.noreply.github.com>
2021-06-28 23:26:21 +0800

    Add files via upload

---
+++ IOS_Build/Assets/Script/PlayButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayButton : MonoBehaviour
+{
+    public GameObject Video;
+    public GameObject Light1;
+    public GameObject Light2;
+    public GameObject ButtonDisabled;
+    Animator StopButton;
+
+    private void Start()
+    {
+        StopButton = ButtonDisabled.GetComponent<Animator>();
+    }
+    public void PlayButtonDown()
+    {
+        Video.gameObject.SetActive(true);
+        Light1.gameObject.SetActive(true);
+        Light2.gameObject.SetActive(true);
+        StopButton.SetBool("Disabled", true);
+    }
+}
---
+++ IOS_Build/Assets/Script/StopButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopButton : MonoBehaviour
+{
+    public GameObject Video;
+    public GameObject Light1;
+    public GameObject Light2;
+    public GameObject ButtonDisabled;
+    Animator PlayButton;
+
+    private void Start()
+    {
+        PlayButton = ButtonDisabled.GetComponent<Animator>();
+    }
+    public void StopButtonDown()
+    {
+        Video.gameObject.SetActive(false);
+        Light1.gameObject.SetActive(false);
+        Light2.gameObject.SetActive(false);
+        PlayButton.SetBool("Disabled", true);
+    }
+}
